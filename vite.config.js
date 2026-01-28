f54b52f868dc662fc294ed90e61bcfc6d785ac05
Maksim <shirant.maksim@yandex.ru>
2026-01-28 19:10:35 +0300

    Update repo name for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/260126/',
+  base: '/stalkonstruksia/',
   plugins: [tailwindcss(), react()],
 })
